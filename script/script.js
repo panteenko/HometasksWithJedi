0666a4f77d94edb9e4601fe70d4e46415d985a35
MacBook <macbook@MacBook-Air-MacBook.local>
2019-07-17 12:25:35 +0300

    shit

--- script/script.js
+++ script/script.js
@@ -1,18 +1,24 @@
 const wrap = document.getElementById('wrapper');
 
 function pushButtonHandler (event) {
+
     if (!event.target.classList.contains ('button')) return;
+
+    if (!document.body.contains ('small-square') && !document.body.classList.contains ('big-square')){
     const square = document.createElement ('div');
+    square.id = 'square';
     square.classList.add ('small-square');
     square.classList.add (event.target.id);
-    
-    if (square.classList.contains('small-square')) {
+    document.body.appendChild(square);
+ }
+    else if (document.body.classList.contains ('small-square')) {
         square.classList.remove('small-square');
         square.classList.add('big-square');
-    } else if (square.classList.contains('big-square')) {
+    } else if (document.body.classList.contains ('big-square')) {
         square.classList.remove('big-square');
     }
-    document.body.appendChild(square);
+    
+
 }
 
 wrap.addEventListener('click', pushButtonHandler);
