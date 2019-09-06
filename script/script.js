ff51b8dada6f503a949be98979bdf95111fbdd04
Alex <a.panteenko@mail.ru>
2019-09-06 11:00:30 +0300

    wtf again

--- script/script.js
+++ script/script.js
@@ -1,27 +1,28 @@
-const wrap = document.getElementById('wrapper'); // получаю доступ к обёртке для кнопок.
-let square; //объявляю переменную в которую буду записывать создаваемый див
+const wrap = document.getElementById('wrapper');
+
 
 function pushButtonHandler (event) {
 
-    if (!event.target.classList.contains ('button')) return; //проверяю где произошло событие, если не на кнопке, а только на обертке, то останавливаю выполнения функции
+    if (!event.target.classList.contains ('button')) return; 
 
-    if (!document.querySelector('.small-square') && !document.querySelector('.big-square')) {  //проверяю если на странице нет элемента ни с одним и ни с другим классом, то выполняю следующий код
+    const square = document.querySelector('.small-square') || document.querySelector('.big-square');
     
-        square = document.createElement('div'); //создаю элемент див и записываю его в переменную square
-        square.classList.add('small-square'); //добавляю своем диву класс
-        wrap.appendChild(square); //апендчайлдом размещаю свой див на страницу, в конец дива обертки
+    if (!square) {
+    
+        square = document.createElement('div');
+        square.classList.add('small-square');
+        wrap.appendChild(square); 
 
-    } else if (document.querySelector('.small-square')) { // если верхнияя проверка не прошла, то проверяю есть ли на странице элемент с классом смол, если есть, то выполняю следующий код
+    } else if (square.classList.contains('.small-square')) { 
 
-        square.classList.remove('small-square'); //удаляю у элемента класс смол
-        square.classList.add('big-square'); // и добавляю ему класс биг
+        square.classList.remove('small-square'); 
+        square.classList.add('big-square'); 
         
-    } else { // в случае не выолнения верхнего кода, выполняю код ниже
+    } else if (square.classList.contains('big-square')) {
 
-        wrap.removeChild(square); // удаляю со страницы элемент square
-
+        wrap.removeChild(square); 
     }
 }
 
 
-wrap.addEventListener('click', pushButtonHandler); //навешиваю на обертку ивент лисенер и говорю ему отслеживать событие клик, и при наступлении события вызвать функцию pushButtonHandler
+wrap.addEventListener('click', pushButtonHandler); 
