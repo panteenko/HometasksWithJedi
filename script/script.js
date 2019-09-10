6dad8b1b8a3b02613c33face8743810c94259f75
Alex <a.panteenko@mail.ru>
2019-09-10 12:28:19 +0300

    done))

--- script/script.js
+++ script/script.js
@@ -36,19 +36,19 @@ function pushButtonHandler (event) {
     if (!event.target.classList.contains ('button')) return; 
 
     let target = event.target.id;
-    let targetClass = '.' + target + 'Square';
-    let square = document.querySelector(targetClass);
+    let targetClass = target + 'Square';
+    let square = document.querySelector('.' + targetClass);
 
     
     if (!square) {
     
         square = document.createElement('div');
         square.classList.add('small-square');
-        square.classList.add(target+'Square');
+        square.classList.add(targetClass);
         wrap.appendChild(square); 
 
     } else if (square.classList.contains('small-square') && square.classList.contains(targetClass)) { 
-        
+
         square.classList.remove('small-square'); 
         square.classList.add('big-square'); 
         
