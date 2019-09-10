86dcebb1975e431ef8a8302efd57f5fa2912c5cc
Alex <a.panteenko@mail.ru>
2019-09-10 11:59:52 +0300

    with colors

--- script/script.js
+++ script/script.js
@@ -1,3 +1,33 @@
+// const wrap = document.getElementById('wrapper');
+
+
+// function pushButtonHandler (event) {
+
+//     if (!event.target.classList.contains ('button')) return; 
+
+//     let square = document.querySelector('.small-square') || document.querySelector('.big-square');
+    
+    
+//     if (!square) {
+    
+//         square = document.createElement('div');
+//         square.classList.add('small-square');
+//         wrap.appendChild(square); 
+
+//     } else if (square.classList.contains('small-square')) { 
+        
+//         square.classList.remove('small-square'); 
+//         square.classList.add('big-square'); 
+        
+//     } else if (square.classList.contains('big-square')) {
+
+//         wrap.removeChild(square); 
+//     }
+// }
+
+
+// wrap.addEventListener('click', pushButtonHandler); 
+
 const wrap = document.getElementById('wrapper');
 
 
@@ -5,20 +35,23 @@ function pushButtonHandler (event) {
 
     if (!event.target.classList.contains ('button')) return; 
 
-    let square = document.querySelector('.small-square') || document.querySelector('.big-square');
+    let target = event.target.id;
+    let square = document.querySelector(target+'Square');
+
     
     if (!square) {
     
         square = document.createElement('div');
         square.classList.add('small-square');
+        square.classList.add(target+'Square');
         wrap.appendChild(square); 
 
-    } else if (square.classList.contains('.small-square')) { 
-
+    } else if (square.classList.contains('small-square') && square.classList.contains(target+'Square')) { 
+        
         square.classList.remove('small-square'); 
         square.classList.add('big-square'); 
         
-    } else if (square.classList.contains('big-square')) {
+    } else if (square.classList.contains('big-square') && square.classList.contains(target+'Square')) {
 
         wrap.removeChild(square); 
     }
@@ -26,3 +59,4 @@ function pushButtonHandler (event) {
 
 
 wrap.addEventListener('click', pushButtonHandler); 
+
